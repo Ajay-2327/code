f4765f0500d16b140faf57257e17b1739d566b43
Ajay-2327 <ajaymalleswarapu@gmail.com>
2025-06-28 12:54:56 +0530

    .

--- resume.js
+++
@@ -1,8 +0,0 @@
-// Animate skill bars on page load
-window.addEventListener('load', () => {
-  const skillLevels = document.querySelectorAll('.skill-level');
-  skillLevels.forEach(bar => {
-    const level = bar.getAttribute('data-level');
-    bar.style.width = level;
-  });
-});
